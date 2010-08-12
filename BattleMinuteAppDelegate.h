a3a1a638b10fdee1a4e822efff4cd59679b5c406
Jeremy <jeremyfoo@gmail.com>
2010-08-12 12:15:45 +0800

    initial import

---
+++ BattleMinuteAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  BattleMinuteAppDelegate.h
+//  BattleMinute
+//
+//  Created by Jeremy Foo on 8/7/10.
+//  Copyright 2010 ORNYX. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BattleMinuteAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ BattleMinuteAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  BattleMinuteAppDelegate.m
+//  BattleMinute
+//
+//  Created by Jeremy Foo on 8/7/10.
+//  Copyright 2010 ORNYX. All rights reserved.
+//
+
+#import "BattleMinuteAppDelegate.h"
+
+@implementation BattleMinuteAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
