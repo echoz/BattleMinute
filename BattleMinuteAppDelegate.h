94f5696269ef32a92047b099f56727b3a005a169
Jeremy F <jeremyfoo@gmail.com>
2011-01-31 23:17:44 +0800

    awesome possum. sends profile information and also deals with new version info

--- BattleMinuteAppDelegate.h
+++ BattleMinuteAppDelegate.h
@@ -7,13 +7,15 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <Sparkle/Sparkle.h>
 
 @interface BattleMinuteAppDelegate : NSObject <NSApplicationDelegate> {
     NSWindow *window;
 	NSWindow *loginWindow;
+	SUUpdater *sparkle;
 }
-
+@property (assign) IBOutlet SUUpdater *sparkle;
 @property (assign) IBOutlet NSWindow *window;
 @property (assign) IBOutlet NSWindow *loginWindow;
-
+-(NSString *)installationId;
 @end
--- BattleMinuteAppDelegate.m
+++ BattleMinuteAppDelegate.m
@@ -10,11 +10,33 @@
 
 @implementation BattleMinuteAppDelegate
 
-@synthesize window, loginWindow;
+@synthesize window, loginWindow, sparkle;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	// Insert code here to initialize your application 
+	[sparkle setSendsSystemProfile:YES];
+}
 
+-(NSString *)installationId {
+    NSString *uuid = [[NSUserDefaults standardUserDefaults] valueForKey:@"uuid"];
+    if (uuid == nil) {
+        uuid_t buffer;
+        char str[37];
+        uuid_generate(buffer);
+        uuid_unparse_upper(buffer, str);        
+        uuid = [NSString stringWithFormat:@"%s", str];
+        NSLog(@"Generated UUID %@", uuid);        
+        [[NSUserDefaults standardUserDefaults] setValue:uuid forKey:@"uuid"];
+    }
+    return uuid;
+}
+
+- (NSArray *)feedParametersForUpdater:(SUUpdater *)updater sendingSystemProfile:(BOOL)sendingProfile {
+    NSArray *keys = [NSArray arrayWithObjects:@"key", @"value", nil];
+    NSArray *parameters = [NSArray arrayWithObjects:
+						   [NSDictionary dictionaryWithObjects: [NSArray arrayWithObjects:@"uuid",[self installationId],nil] forKeys:keys], nil
+						   ];
+    return parameters;
 }
 
 @end
